ebac65af0c3319f7e64c271efdbd41613ffafb5a
Thiago Aldrin Marques da Silva <thiago.amsilva@sp.senac.br>
2024-06-27 13:14:54 -0300

    Inicio da revisao

---
+++ src/main/java/com/tontonX/Spring3Mvc/Spring3MvcApplication.java
@@ -0,0 +1,13 @@
+package com.tontonX.Spring3Mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3MvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tontonX/Spring3Mvc/Spring3MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tontonX.Spring3Mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
